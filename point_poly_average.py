a384a936bca18d8a21928f08f8d74848b74c5714
Gaynor Astbury <contact@gaynorastbury.com>
2021-12-06 16:16:52 +0000

    Update point_poly_average.py

--- point_poly_average.py
+++ point_poly_average.py
@@ -12,10 +12,10 @@ Python3 script
 import pandas
 import geopandas
 
-noquals = geopandas.read_file('points.shp') #input points to a geodataframe
-walkrail = geopandas.read_file('polygons.shp') #input polygons to a geodataframe
+point_gdf = geopandas.read_file('points.shp') #input points to a geodataframe
+poly_gdf = geopandas.read_file('polygons.shp') #input polygons to a geodataframe
 
-join_left_df = geopandas.sjoin(noquals, walkrail, how='left', op='intersects') # find and join polygon attributes to points using spatial join
+join_left_df = geopandas.sjoin(point_gdf, poly_gdf, how='left', op='intersects') # find and join polygon attributes to points using spatial join
 mean_df = join_left_df.groupby(['IDtogroupby'])['columnvalue'].sum().reset_index() # use pandas to calculate the average with a groupby
 merged = polygons.merge(mean_df, on='IDtogroupby') # join the resulting averages back to the original polygons
 merged.to_file('final_result.shp') # export the results as a polygon file if needed
