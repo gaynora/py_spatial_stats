7e4601990af187b2a69b7cd2d537def82023c189
Gaynor Astbury <contact@gaynorastbury.com>
2023-06-13 16:04:36 +0100

    Update append_via_list.py

--- append_via_list.py
+++ append_via_list.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun  6 09:18:39 2023
+Appends multiple dataframes from seperate input files using a loop within a function.
+All input table schemas must match.
+Python 3 script.
 
 @author: gaynor.astbury
 """
 
 import pandas
 
-table1_df = pandas.read_csv('table1.csv') # keep adding to this one and use as global variable?
+table1_df = pandas.read_csv('table1.csv') 
 table2_df = pandas.read_csv('table2.csv')
 table3_df = pandas.read_csv('table3.csv')
 
-start_df = pandas.DataFrame(columns=['Columna', 'Columnb', 'Columnc'])
+start_df = pandas.DataFrame(columns=['Columna', 'Columnb', 'Columnc']) # empty dataframe to start - schema may need editing
 dflist = [table1_df, table2_df, table3_df]
 
 def append_list(x):
-    for _ in dflist: #this would be a list of gdfs?
+    for _ in dflist: 
         x = start_df.append(x)
     return x
 
