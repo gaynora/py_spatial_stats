468741eaef8fe97fd58929654f15c0def3189ba2
Gaynor Astbury <contact@gaynorastbury.com>
2023-07-28 09:04:22 +0100

    Update owslib_boilerplate.py

--- owslib_boilerplate.py
+++ owslib_boilerplate.py
@@ -13,8 +13,8 @@ from requests import Request
 # READ WFS CAPABILITIES AND METADATA
 url = "https://ogc.bgs.ac.uk/digmap625k_gsml_insp_gs/wfs" # URL for WFS backend - to be edited 
 wfs = WebFeatureService(url=url) # Initialize
-print(wfs.identification.title) # Service provider 
-print(wfs.version) # Get WFS version
+print(wfs.identification.title) # Service provider for a sense check
+v = wfs.version # fetches the version required as a varying request parameter
 #print([operation.name for operation in wfs.operations]) # Available methods
 #print(list(wfs.contents)) # Available data layers
 #for layer, meta in wfs.items(): # Print all metadata of all layers
@@ -22,22 +22,23 @@ print(wfs.version) # Get WFS version
     
 # READ THE DATA INTO GEODATAFRAME:
 layer_name = list(wfs.contents)[-1] # Fetch the last available layer (as an example) 
-params = dict(service='WFS', version="1.0.0", request='GetFeature', # Specify the parameters for fetching the data
+params = dict(service='WFS', version=v, request='GetFeature', # Specify the parameters for fetching the data
       typeName=layer_name, outputFormat='json', count=100, startIndex=0) # Count: amount of rows to return, startIndex: offset to start returning rows
 
 wfs_request_url = Request('GET', url, params=params).prepare().url # Parse the URL with parameters
 
 data = gpd.read_file(wfs_request_url) # Read data from URL
-data.to_file('output.shp')
+data.to_file('output1.shp')
 
 
 
 #FUNCTIONAL APPROACH
 def get_wfs(url):
    
-    wfs = WebFeatureService(url) # initialise    
+    wfs = WebFeatureService(url) # initialise  
+    v = wfs.version # fetches the version required as a varying request parameter
     layer_name = list(wfs.contents)[-1] # Fetch the last available layer (as an example)    
-    params = dict(service='WFS', version="1.0.0", request='GetFeature', typeName=layer_name, outputFormat='json', count=100, startIndex=0) # Specify the parameters for fetching the data Count: amount of rows to return, startIndex: offset to start returning rows    
+    params = dict(service='WFS', version=v, request='GetFeature', typeName=layer_name, outputFormat='json', count=100, startIndex=0) # Specify the parameters for fetching the data Count: amount of rows to return, startIndex: offset to start returning rows    
     wfs_request_url = Request('GET', url, params=params).prepare().url # Parse the URL with parameters    
     wfs_gdf = gpd.read_file(wfs_request_url) # return wfs_request_url
     return wfs_gdf
